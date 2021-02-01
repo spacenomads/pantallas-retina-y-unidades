1096a4bed40d8343a82617ed3960e0d69a82b8af
Carlos Mañas <carlos@spacenomads.com>
2021-02-01 14:06:05 +0100

    Improve indentation and spaces

--- js/snitch.js
+++ js/snitch.js
@@ -1,16 +1,28 @@
 const app = document.querySelector('.js__app');
 const snitch = app.querySelector('.js__snitch');
 
+
+
+
+
 function getBrowserSize() {
   const width = window.innerWidth;
   const height = window.innerHeight;
   return {width, height};
 }
 
+
+
+
+
 function paintBrowserSize(el) {
   const {width, height} = getBrowserSize();
   el.innerHTML = `Ventana: <b>${width}</b>x<b>${height}</b> px`;
 }
+
+
+
+
 
 paintBrowserSize(snitch);
 window.addEventListener('resize', ()=>{
